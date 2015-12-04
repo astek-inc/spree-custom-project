177e6285b76df6df083ae953dd108a0e69be8948
Edwin Horneij <edwin@astekwallcovering.com>
2015-12-04 14:23:29 -0800

    Using Paranoia gem to flag items as deleted instead of removing them from DB.

--- app/controllers/spree/admin/custom_projects_controller.rb
+++ app/controllers/spree/admin/custom_projects_controller.rb
@@ -22,15 +22,15 @@ module Spree
       def collection
         return @collection if @collection.present?
         params[:q] ||= {}
-        #params[:q][:deleted_at_null] ||= "1"
+        params[:q][:deleted_at_null] ||= "1"
 
         params[:q][:s] ||= "created_at desc"
         @collection = super
         # Don't delete params[:q][:deleted_at_null] here because it is used in view to check the
         # checkbox for 'q[deleted_at_null]'. This also messed with pagination when deleted_at_null is checked.
-        # if params[:q][:deleted_at_null] == '0'
-        #   @collection = @collection.with_deleted
-        # end
+        if params[:q][:deleted_at_null] == '0'
+          @collection = @collection.with_deleted
+        end
         # @search needs to be defined as this is passed to search_form_for
         # Temporarily remove params[:q][:deleted_at_null] from params[:q] to ransack products.
         # This is to include all products and not just deleted products.
