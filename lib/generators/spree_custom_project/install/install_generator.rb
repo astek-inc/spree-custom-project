763fa20a853e914805e53df9b472a5a8093517d9
Edwin Horneij <edwin@astekwallcovering.com>
2016-10-20 09:08:39 -0700

    Move CSS to separate file

--- lib/generators/spree_custom_project/install/install_generator.rb
+++ lib/generators/spree_custom_project/install/install_generator.rb
@@ -8,11 +8,10 @@ module SpreeCustomProject
       #   append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_custom_project\n"
       #   append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_custom_project\n"
       # end
-      #
-      # def add_stylesheets
-      #   inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_custom_project\n", :before => /\*\//, :verbose => true
-      #   inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_custom_project\n", :before => /\*\//, :verbose => true
-      # end
+
+      def add_stylesheets
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_custom_project\n", :before => /\*\//, :verbose => true
+      end
 
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_custom_project'
