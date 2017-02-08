5da39db7627a464b48be725564ec9852b7883e50
Edwin Horneij <edwin@astekwallcovering.com>
2017-02-08 13:23:36 -0800

    New original definition for override

--- app/overrides/add_custom_projects_to_admin_menu.rb
+++ app/overrides/add_custom_projects_to_admin_menu.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(
-    :original => 'b511780df47e0ebcdfe1aeb87c0143df8d2f4985',
+    :original => '30e04e20633b85d74dcb22f781487425611b22b1',
     :virtual_path => 'spree/layouts/admin',
     :name => 'add_custom_projects_to_admin_menu',
     :insert_bottom => '[data-hook="admin_tabs"]',
