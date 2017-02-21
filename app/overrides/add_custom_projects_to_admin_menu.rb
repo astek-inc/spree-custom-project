90cc0bb35d087950e7a05a9c150ba76c2b12279f
Edwin Horneij <edwin@astekwallcovering.com>
2017-02-21 14:33:30 -0800

    Update original for override

--- app/overrides/add_custom_projects_to_admin_menu.rb
+++ app/overrides/add_custom_projects_to_admin_menu.rb
@@ -1,9 +1,9 @@
 Deface::Override.new(
-    :original => '30e04e20633b85d74dcb22f781487425611b22b1',
-    :virtual_path => 'spree/layouts/admin',
-    :name => 'add_custom_projects_to_admin_menu',
-    :insert_bottom => '[data-hook="admin_tabs"]',
-    :text => '<ul class="nav nav-sidebar">
-        <%= tab Spree.t(:custom_projects), :url => admin_custom_projects_path, :icon => \'heart-empty\' %>
+    original: '4afaf76e759952bb4b5ca9c5718c8712da432442',
+    virtual_path: 'spree/layouts/admin',
+    name: 'add_custom_projects_to_admin_menu',
+    insert_bottom: '[data-hook="admin_tabs"]',
+    text: '<ul class="nav nav-sidebar">
+        <%= tab Spree.t(:custom_projects), url: admin_custom_projects_path, icon: \'heart-empty\' %>
       </ul>'
 )
